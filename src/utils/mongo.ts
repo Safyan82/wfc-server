920c97ac3e1332b4ae3bd00ee635229053b2f4b6
Muhammad Safyan Mehar <msafyan46@gmail.com>
2023-08-15 04:20:43 +0100

    connection string updated

--- src/utils/mongo.ts
+++ src/utils/mongo.ts
@@ -3,10 +3,12 @@ import config from "config";
 
 export async function connection() {
     try{
+        console.log("connecting")
         await mongoose.connect(config.get('dbUri'));
         console.log("connected ...");
     }
     catch(err){
+        console.log("error")
         console.log(err);
         process.exit(1);
     }
