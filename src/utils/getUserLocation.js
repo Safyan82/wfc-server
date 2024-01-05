cbedd460691c96fd705fff1b561949d1dad42914
Muhammad Safyan Mehar <msafyan46@gmail.com>
2024-01-05 05:51:18 +0000

    location ip

--- src/utils/getUserLocation.js
+++ src/utils/getUserLocation.js
@@ -1,7 +1,7 @@
 const ipLocation = require("iplocation");
  
 const getLocation =  async(ip) => {
-    const location = await ipLocation("172.217.167.78");
+    const location = await ipLocation(ip);
     console.log(location);
     return location
     //=> { latitude: -33.8591, longitude: 151.2002, region: { name: "New South Wales" ... } ... }
